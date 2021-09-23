1b534c4c342516d82c999c7a0624da2a45393663
sir William <williamfr0615@gmail.com>
2021-09-22 22:08:35 -0300

    Comments
    for the shop class

--- TextedBased RPG/Legacy work/Shop.cs
+++ TextedBased RPG/Legacy work/Shop.cs
@@ -9,10 +9,11 @@ namespace TextedBased_RPG
     class Shop : InteractiveArea
     {
         private int shopTax = 3; // added to sold items
-        private ItemManager items;
-        public List<ITEM> itemsInShop = new List<ITEM>();
-        private string itemsToBuy;
-        private string itemsToSell;
+        private ItemManager items; 
+        public List<ITEM> itemsInShop = new List<ITEM>(); 
+        private string itemsToBuy; // string that shows what player can  buy  at the shop
+        private string itemsToSell; // ^^ but                            sell
+
         //constructor
         public Shop(string name, string DesiredDialogue, int x, int y, ItemManager itemsTarget, List<ITEM> itemsForStock) : base(name, DesiredDialogue)
         {
@@ -32,13 +33,13 @@ namespace TextedBased_RPG
         {
             if (itemsInShop.Contains(ITEM.POTION))
             {
-                itemsToBuy = itemsToBuy + $"\n{items.GetItemName(ITEM.POTION)[0]}) {items.GetItemName(ITEM.POTION)} - {items.GetItemPrice(ITEM.POTION)}$";
-                itemsToSell = itemsToSell + $"\n{items.GetItemName(ITEM.POTION)[0]}) {items.GetItemName(ITEM.POTION)} - {items.GetItemPrice(ITEM.POTION) - shopTax}$";
+                itemsToBuy = itemsToBuy + $"\n{items.GetItemName(ITEM.POTION)[0]}) {items.GetItemName(ITEM.POTION)} - {items.GetItemPrice(ITEM.POTION) + shopTax}$";
+                itemsToSell = itemsToSell + $"\n{items.GetItemName(ITEM.POTION)[0]}) {items.GetItemName(ITEM.POTION)} - {items.GetItemPrice(ITEM.POTION) }$";
             }
             if (itemsInShop.Contains(ITEM.RAFT))
             {
-                itemsToBuy = itemsToBuy + $"\n{items.GetItemName(ITEM.RAFT)[0]}) {items.GetItemName(ITEM.RAFT)} - {items.GetItemPrice(ITEM.RAFT)}$";
-                itemsToSell = itemsToSell + $"\n{items.GetItemName(ITEM.RAFT)[0]}) {items.GetItemName(ITEM.RAFT)} - {items.GetItemPrice(ITEM.RAFT) - shopTax}$";
+                itemsToBuy = itemsToBuy + $"\n{items.GetItemName(ITEM.RAFT)[0]}) {items.GetItemName(ITEM.RAFT)} - {items.GetItemPrice(ITEM.RAFT) + shopTax}$";
+                itemsToSell = itemsToSell + $"\n{items.GetItemName(ITEM.RAFT)[0]}) {items.GetItemName(ITEM.RAFT)} - {items.GetItemPrice(ITEM.RAFT)}$";
             }
             if (itemsInShop.Contains(ITEM.SWORD))
             {
@@ -49,7 +50,7 @@ namespace TextedBased_RPG
                 itemsToBuy = itemsToBuy + $"\n{items.GetWeaponName(ITEM.BOW)[0]}) {items.GetWeaponName(ITEM.BOW)} / ATTK^{items.GetWeaponAttack(ITEM.BOW)} - {items.GetItemPrice(ITEM.BOW)}$";
             }
         }
-        private void BuyOutput()
+        private void BuyOutput() // Exchanges Money for a taxed item
         {
 
             if (input == (ConsoleKey)items.GetWeaponName(ITEM.SWORD)[0] && itemsInShop.Contains(ITEM.SWORD))
@@ -57,10 +58,10 @@ namespace TextedBased_RPG
                 if (user.Money >= items.GetItemPrice(ITEM.SWORD)) // buy method
                 {
                     Console.Clear();
-                    lastAction = $"you purchase a {items.GetWeaponName(ITEM.SWORD)}";
+                    lastAction = $"you purchase a {items.GetWeaponName(ITEM.SWORD)} for {items.GetItemPrice(ITEM.SWORD) + shopTax}";
                     int attackChange = user.baseAttack + items.GetWeaponAttack(ITEM.SWORD);
                     Console.WriteLine($"purchasing a {items.GetWeaponName(ITEM.SWORD)} \nwill change your attack to " + attackChange + " from " + user.attack + ".");
-                    user.CashSpend(items.GetItemPrice(ITEM.SWORD));
+                    user.CashSpend(items.GetItemPrice(ITEM.SWORD) + shopTax);
                     user.WeaponChange(ITEM.SWORD);
                     Console.ReadKey(true);
                 }
@@ -74,10 +75,10 @@ namespace TextedBased_RPG
                 if (user.Money >= items.GetItemPrice(ITEM.BOW))
                 {
                     Console.Clear();
-                    lastAction = $"you purchased a {items.GetWeaponName(ITEM.BOW)}";
+                    lastAction = $"you purchased a {items.GetWeaponName(ITEM.BOW)} for {items.GetItemPrice(ITEM.BOW) + shopTax}";
                     int attackChange = user.baseAttack + items.GetWeaponAttack(ITEM.BOW);
                     Console.WriteLine($"purchasing a {items.GetWeaponName(ITEM.BOW)} \nwill change your attack to " + attackChange + " from " + user.attack + ".");
-                    user.CashSpend(items.GetItemPrice(ITEM.BOW));
+                    user.CashSpend(items.GetItemPrice(ITEM.BOW) + shopTax);
                     user.WeaponChange(ITEM.BOW);
                     Console.ReadKey(true);
                 }
@@ -90,8 +91,8 @@ namespace TextedBased_RPG
             {
                 if (user.Money >= items.GetItemPrice(ITEM.POTION))
                 {
-                    lastAction = $"you buy a {items.GetItemName(ITEM.POTION)} with {items.GetItemPrice(ITEM.POTION)}$";
-                    user.CashSpend(items.GetItemPrice(ITEM.POTION));
+                    lastAction = $"you buy a {items.GetItemName(ITEM.POTION)} with {items.GetItemPrice(ITEM.POTION) + shopTax}$";
+                    user.CashSpend(items.GetItemPrice(ITEM.POTION) + shopTax);
                     user.potionNumber++;
                 }
                 else
@@ -103,8 +104,8 @@ namespace TextedBased_RPG
             {
                 if (user.Money >= items.GetItemPrice(ITEM.RAFT) && !user.hasBoat)
                 {
-                    lastAction = $"You buy a {items.GetItemName(ITEM.RAFT)} with {items.GetItemPrice(ITEM.RAFT)}$";
-                    user.CashSpend(items.GetItemPrice(ITEM.RAFT));
+                    lastAction = $"You buy a {items.GetItemName(ITEM.RAFT)} with {items.GetItemPrice(ITEM.RAFT) + shopTax}$";
+                    user.CashSpend(items.GetItemPrice(ITEM.RAFT) + shopTax);
                     user.hasBoat = true;
                 }
                 else if (user.hasBoat)
@@ -117,14 +118,14 @@ namespace TextedBased_RPG
                 }
             }
         }
-        private void SellOutput()
+        private void SellOutput() // Exchanges Items for Money
         {
             if (input == (ConsoleKey)items.GetItemName(ITEM.POTION)[0] && itemsInShop.Contains(ITEM.POTION))
             {
                 if (user.potionNumber >= 1)
                 {
-                    lastAction = "You sold a Potion";
-                    user.CashGain(items.GetItemPrice(ITEM.POTION) - shopTax);
+                    lastAction = $"You sold a Potion for {items.GetItemPrice(ITEM.POTION)}";
+                    user.CashGain(items.GetItemPrice(ITEM.POTION));
                     user.potionNumber--;
                 }
                 else
@@ -136,8 +137,8 @@ namespace TextedBased_RPG
             {
                 if (user.hasBoat)
                 {
-                    lastAction = "You sold your boat";
-                    user.CashGain(items.GetItemPrice(ITEM.RAFT) - shopTax);
+                    lastAction = $"You sold your boat for {items.GetItemPrice(ITEM.RAFT)}";
+                    user.CashGain(items.GetItemPrice(ITEM.RAFT));
                     user.hasBoat = false;
                 }
                 else 
@@ -149,8 +150,8 @@ namespace TextedBased_RPG
             {
                 if (user.HeldWeapon != ITEM.NULL) // sell method
                 {
-                    lastAction = $"You sold a {items.GetWeaponName(user.HeldWeapon)} for {items.GetItemPrice(user.HeldWeapon) - shopTax}$";
-                    user.CashGain(items.GetItemPrice(user.HeldWeapon) - shopTax);
+                    lastAction = $"You sold a {items.GetWeaponName(user.HeldWeapon)} for {items.GetItemPrice(user.HeldWeapon) }$";
+                    user.CashGain(items.GetItemPrice(user.HeldWeapon));
                     user.Weapon = "None";
                     user.HeldWeapon = ITEM.NULL;
                     user.attack = -5;
@@ -163,7 +164,7 @@ namespace TextedBased_RPG
         }
 
         //public methods
-        private void Shopping()
+        private void Shopping() // Shopping menu
         {
             Console.Clear();
             lastAction = "BUY SOMETHING, :D HA,HA,HA.";
@@ -187,7 +188,7 @@ namespace TextedBased_RPG
                 Console.Clear();
             }
         }
-        private void Selling()
+        private void Selling() // Selling menu
         {
             bool selling = true;
             while (selling) 
@@ -214,7 +215,7 @@ namespace TextedBased_RPG
             }
             Console.Clear();
         }
-        public void EnterShop()
+        public void EnterShop() // Shop Lobby
         {
             lastAction = "";
             for (int x = 0; x < 1;)
